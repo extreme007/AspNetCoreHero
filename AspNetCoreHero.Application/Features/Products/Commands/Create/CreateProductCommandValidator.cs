2d96a3a2c45049062bd32b9659f1050334fbc6e5
annh1 <annh1@ocb.com.vn>
2020-12-15 11:40:17 +0700

    Update

--- AspNetCoreHero.Application/Features/Products/Commands/Create/CreateProductCommandValidator.cs
+++ AspNetCoreHero.Application/Features/Products/Commands/Create/CreateProductCommandValidator.cs
@@ -1,20 +1,25 @@
-﻿using FluentValidation;
+﻿using AspNetCoreHero.Application.Interfaces.Repositories;
+using FluentValidation;
 using System;
 using System.Collections.Generic;
 using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
 
 namespace AspNetCoreHero.Application.Features.Products.Commands.Create
 {
     public class CreateProductCommandValidator : AbstractValidator<CreateProductCommand>
     {
+        private readonly IProductRepositoryAsync _productRepository;
 
-        public CreateProductCommandValidator()
+        public CreateProductCommandValidator(IProductRepositoryAsync productRepository)
         {
+            this._productRepository = productRepository;
             RuleFor(p => p.Barcode)
                 .NotEmpty().WithMessage("{PropertyName} is required.")
                 .NotNull()
                 .MaximumLength(50).WithMessage("{PropertyName} must not exceed 50 characters.")
-                .WithMessage("{PropertyName} already exists.");
+                .MustAsync(IsUniqueBarcode).WithMessage("{PropertyName} already exists.");
 
             RuleFor(p => p.Name)
                 .NotEmpty().WithMessage("{PropertyName} is required.")
@@ -22,6 +27,11 @@ namespace AspNetCoreHero.Application.Features.Products.Commands.Create
                 .MinimumLength(10)
                 .MaximumLength(50).WithMessage("{PropertyName} must not exceed 50 characters.");
 
+        }
+
+        private async Task<bool> IsUniqueBarcode(string barcode, CancellationToken cancellationToken)
+        {
+            return await _productRepository.IsUniqueBarcodeAsync(barcode);
         }
     }
 }
